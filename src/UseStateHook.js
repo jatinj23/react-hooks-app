e37c5b7363fb112fa625a62862e1ed187995c1e9
jatin jagnani <jatinjagnani@jatins-MacBook-Pro.local>
2020-09-19 11:38:22 +0530

    initial commit

---
+++ src/UseStateHook.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './main-style.css'
+
+function UseStateHook() {
+    const [count, setCount] = useState(0);
+
+    // function initialCount() {
+    //     console.log("Run Function")
+    //     return 4;
+    // }
+
+    function decrementCount() {
+        setCount(prevCount => prevCount - 1);
+    }
+
+    function incrementCount() {
+        setCount(prevCount => prevCount + 1);
+    }
+
+    return (
+        <div>
+            <h1>Use State Hook</h1>
+            <button onClick={decrementCount}>-</button>
+            <span>{count}</span>
+            <button onClick={incrementCount}>+</button>
+        </div>
+    )
+}
+
+export default UseStateHook
